bdf91a3701e9b5309667d61c38a4347db6fa5599
CONROY Aedan <aconroy@AltranUK.local>
2020-09-14 11:05:03 +0100

    multiples of 17

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,19 +1,29 @@
-for i in range(200):
+for i in range(300):
     myString = ""
     if i % 3 == 0:
-        myString = myString + "Fizz"
+        myString = myString + "-Fizz"
     if i % 5 == 0:
-        myString = myString + "Buzz"
+        myString = myString + "-Buzz"
     if i % 7 == 0:
-        myString = myString + "Bang"
+        myString = myString + "-Bang"
     if i % 11 == 0:
-        myString = "Bong"
+        myString = "-Bong"
     if i % 13 == 0:
         index = myString.find("B")
         if index == -1:
-            myString = myString + "Fezz"
+            myString = myString + "-Fezz"
         else:
-            myString = myString[:index] + "Fezz" + myString[index:]
+            myString = myString[:index-1] + "-Fezz" + myString[index-1:]
+    # if i % 17 == 0:
+    #     myString.split(" ")
+
+    myList = myString.split("-")
+    if i % 17 == 0:
+        myList.reverse()
+    myString = str(i) + ": "
+    for word in myList:
+        myString = myString + word
+
     if myString == "":
         myString = i
     #myString = str(i) + ": " + myString
