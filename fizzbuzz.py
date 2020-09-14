5ecc82f4971cddb300bf06c3f754bc7b4b0b3ec8
CONROY Aedan <aconroy@AltranUK.local>
2020-09-14 10:51:31 +0100

    Fezz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,20 @@
-for i in range(100):
-    str = ""
+for i in range(200):
+    myString = ""
     if i % 3 == 0:
-        str = str + "Fizz"
+        myString = myString + "Fizz"
     if i % 5 == 0:
-        str = str + "Buzz"
+        myString = myString + "Buzz"
     if i % 7 == 0:
-        str = str + "Bang"
+        myString = myString + "Bang"
     if i % 11 == 0:
-        str = "Bong"
-
-    if str == "":
-        str = i
-    print(str)
+        myString = "Bong"
+    if i % 13 == 0:
+        index = myString.find("B")
+        if index == -1:
+            myString = myString + "Fezz"
+        else:
+            myString = myString[:index] + "Fezz" + myString[index:]
+    if myString == "":
+        myString = i
+    #myString = str(i) + ": " + myString
+    print(myString)
